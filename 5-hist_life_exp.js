99ba78c2c93e3d0e4172c191a7d882a2bd815a18
Ray Huber <raymond@raymondhuber.com>
2016-08-09 20:38:34 -0400

    initial commit

---
+++ 5-hist_life_exp.js
@@ -0,0 +1,29 @@
+// Your code here.
+function every(elements, action){
+  var isTrue = true;
+  for(var i = 0; i<elements.length; i++){
+    if (action(elements[i]) == false){
+      isTrue = false;
+    }
+  }
+  return isTrue;
+}
+
+function some(elements, action){
+  var isTrue = false; // assume false, have to check to make sure some are true
+  for(var i = 0; i<elements.length; i++){
+    if (action(elements[i]) == true){
+      isTrue = true;
+    }
+  }
+  return isTrue;
+}
+
+console.log(every([NaN, NaN, NaN], isNaN));
+// → true
+console.log(every([NaN, NaN, 4], isNaN));
+// → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
+// → false
